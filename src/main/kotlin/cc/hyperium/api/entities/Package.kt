104118efc3c90a1f13c9da49ddeebc13f27245cf
VRCube <vrcproxz@gmail.com>
2018-03-03 16:33:05 +0100

    purchases response + Package enum

---
+++ src/main/kotlin/cc/hyperium/api/entities/Package.kt
@@ -0,0 +1,7 @@
+package cc.hyperium.api.entities
+
+enum class Package(id: Int) {
+    LEVELHEAD(2709838),
+    EARLY_BIRD(2888258);
+    var id = id
+}
---
+++ src/main/kotlin/cc/hyperium/api/responses/PurchasesResponse.kt
@@ -0,0 +1,8 @@
+package cc.hyperium.api.responses
+
+import cc.hyperium.api.entities.Package
+import cc.hyperium.api.requests.PurchasesRequest
+
+class PurchasesResponse(r: PurchasesRequest, pack: Array<Package>) : APIResponse<PurchasesRequest>(r){
+    var pack: Array<Package> = pack
+}
